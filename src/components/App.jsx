166586c256f55fb80cd37ea9e3d7c8e8784b4beb
YCTSAI <yctsai116@gmail.com>
2020-07-13 20:43:22 +0200

    modularize note
    By property and mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
 
 function App() {
     return (<div>
         <Header />
-        <Note />
+        {notes.map(entry =>
+            <Note
+                key={entry.key}
+                title={entry.title}
+                content={entry.content}
+            />
+        )}
         <Footer />
     </div>)
 }
