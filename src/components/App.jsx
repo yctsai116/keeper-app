dd0e91389caf6d99f822732586a28720b1a71e5b
YCTSAI <yctsai116@gmail.com>
2020-07-16 20:54:27 +0200

    Keep track of the title and content.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 
 function App() {
     return (<div>
         <Header />
+        <CreateArea />
         {notes.map(entry =>
             <Note
                 key={entry.key}
